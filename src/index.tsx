919aef19d79bdbe66494d636b9e74ec14db38d32
Appazov <anvar978@yandex.ru>
2022-09-05 14:14:21 +0400

    Add gh-pages

--- src/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import 'assets/styles/global.scss'
-import App from './App'
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
