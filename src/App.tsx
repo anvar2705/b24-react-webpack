54f423993371168576eb92162dbf39364b46ada6
Appazov <anvar978@yandex.ru>
2022-09-05 13:33:53 +0400

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import s from './App.module.css'
+import Button from 'components/Button/Button'
+
+const App = () => {
+  return (
+    <div className={s.root}>
+      App
+      <Button>dscds</Button>
+    </div>
+  )
+}
+
+export default App
